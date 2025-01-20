17933723ee83bf26f39dfdc96d5b394564607a44
Anton <113907270+s224268@users.noreply.github.com>
2025-01-20 02:54:43 +0100

    Removed the crockpie

--- app/src/main/java/dtu/dk/introDistributedProjectApp/mvvm/game/round/RoundUiModel.kt
+++ app/src/main/java/dtu/dk/introDistributedProjectApp/mvvm/game/round/RoundUiModel.kt
@@ -10,13 +10,9 @@ import dtu.dk.introDistributedProjectApp.ui.theme.MyrtleGreen
 
 data class RoundUiModel(
     val currentScore: Int = 2,
-    val currentQuestion: Question = Question("When a man has vigorous, unprotected intercourse with a woman. " +
-            "He then her eats out to the point of vomiting. After filling her vagina with vomit, " +
-            "the man then uses his home economics skills to sew shut the woman's vagina. " +
-            "After several weeks of \"brewing,\" the man, or perhaps another very impatient man, " +
-            "cuts open the stitches, and uses the mixture to make a pie and eat it with mash potato", listOf("Sad Meep", "Crockpie", "Guston Guston"), 1),
+    val currentQuestion: Question = Question("Loading...", listOf("Option one", "Option two", "Option three"), 1),
     val currentRound: Int = 3,
-    val secondsLeft: Int = 14,
+    val secondsLeft: Int = 30,
     val finished: Boolean = false,
     val buttonColors: List<Color> = listOf(ChineseViolet, AirForceBlue, MyrtleGreen),
     val selectedAnswer: Int = 0,
